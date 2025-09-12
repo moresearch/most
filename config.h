e48d9c434209dfce9296837bfbaef462a70fe1ee
moresearch <uni.m.a.fouad@gmail.com>
2025-09-12 17:45:21 -0400

    bee

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "Share Tech Mono:size=16:antialias=true:autohint=true";
+static char *font = "Share Tech Mono:size=12:antialias=true:autohint=true";
 
 //static char *font = "Terminus:size=16:antialias=true:autohint=true";
 //static char *font = "Terminus:size=12:antialias=true:autohint=true";
